4ff11a04ca05f79f01f22b44e191f9677bb607ac
Cristiam Da Silva <cristiam86@gmail.com>
2024-10-17 23:30:12 +0200

    docs: Update football_prediction_market.py commented llm call

--- contracts/football_prediction_market.py
+++ contracts/football_prediction_market.py
@@ -73,6 +73,7 @@ class FootballPredictionMarket(IContract):
         match_resolution_url = (
             "https://www.bbc.com/sport/football/scores-fixtures/" + game_date
         )
+        # commented to allow to test matches in the past.
         # match_status = await self._check_match(match_resolution_url, team1, team2)
 
         # if int(match_status["winner"]) > -1:
